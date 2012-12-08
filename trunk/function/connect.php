4f6b43a885d92d5649f7e3859728ef5cf292b18f
yogakun126@gmail.com <yogakun126@gmail.com@76685d5f-0976-b1f4-9368-13d1857391ea>
2012-12-08 12:56:29 +0000

    buat connect.php --> udah tak update trus fungsiaddanggota.php --> berhasil dijalankan :)
    semangat guys

--- trunk/function/connect.php
+++ trunk/function/connect.php
@@ -3,6 +3,7 @@ $conn = mysql_connect('localhost', 'root', '');
 if (!$conn) {
 	die('eror connection '.mysql_error());
 	}
+mysql_select_db("srikandi");
 if(!mysql_select_db("srikandi")) {
 	die("cannot select your database");
 	}
